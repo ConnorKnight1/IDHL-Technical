1878ecacd51c85868daf718b6b41d79b1b12c730
connor <connor@applieddigital.co.uk>
2025-06-04 12:07:12 +0100

    Project Init

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # IDHL-Technical
 
-# Project Start 
+# Webpack
+
+## Project uses Webpack to handle multiple front-end workflows. 
+
+# 1. Module Bundling - compiles JS files into single bundle.js
+# 2. SASS Precompilation - Converts scss files to CSS using sass loader.
+# 3. Dev Server - webpack-dev-server servers site with live reloading.
+# 4. HTML Handling - Auto injects final CSS/JS into index.html using html-webpack-plugin
+# 5. Output Management - Cleans and writes compiled files to dist/ folder.
+
+### NPM Scripts
+
+# npm run start
+# 1. Starts Development server using webpack-dev-server 
+# 2. Watches for file changes 
+# 3. Rebuilds / Reloads browser automatically if there's changes to files.
+
+# npm run build
+# 1. Runs webpack in either 'dev' or 'prod'
+# 2. Read config from webpack.config.js 
+# 3. Complies all source files 
+# 4. Outputs final files within dist/ directory 
+
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js', // Webpack knows where your app starts
+  output: {
+    filename: 'bundle.js', // Final compiled JS
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Deletes old files on each build
+  },
+  module: {
+    rules: [
+      { // JS transpilation
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      // Styles written in SCSS and compiled via Webpack sass loader.
+      { // SASS processing
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Load template from src // Inject assets into HTML //
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+    hot: true,
+  },
+  mode: 'development',
+};
